1ef1ca4f7d087a27eaee6648573364a09d322df9
Fabiano Cunha <cunhasb@aol.com>
2017-11-09 17:51:31 -0500

    Done.

--- db/migrate/20171109205316_create_join_table_rides.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableRides < ActiveRecord::Migration
-  def change
-    create_join_table :taxis, :passengers do |t|
-      # t.index [:taxi_id, :passenger_id]
-      # t.index [:passenger_id, :taxi_id]
-    end
-  end
-end
