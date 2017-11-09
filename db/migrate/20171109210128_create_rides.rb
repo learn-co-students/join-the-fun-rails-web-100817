9c5c066c9560d9897363b1c9d09df04ba8fa676a
Fabiano Cunha <cunhasb@aol.com>
2017-11-09 17:44:45 -0500

    Done.

--- db/migrate/20171109210128_create_rides.rb
+++ db/migrate/20171109210128_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-      t.timestamps null: false
+      t.integer :taxi_id
+      t.integer :passenger_id
     end
   end
 end
